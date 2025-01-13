7df959d1f83d1d261d6e32de7f2363f777e750ff
N0161651 <erik.brooks@libertymutual.com>
2025-01-13 09:31:23 -0500

    r1

--- rule.ts
+++ rule.ts
@@ -1,12 +1,16 @@
-PS C: \Users\N0161651\REPO\ronin - auto - playwright - automation > node scripts / generateRules.mjs
-file:///C:/Users/N0161651/REPO/ronin-auto-playwright-automation/scripts/generateRules.mjs:14
-interface Condition {
-^^^^^^^^^
+PS C: \Users\N0161651\REPO\ronin - auto - playwright - automation > node scripts / generateRules.ts
+node: internal / modules / esm / get_format: 160
+throw new ERR_UNKNOWN_FILE_EXTENSION(ext, filepath);
+        ^
 
-    SyntaxError: Unexpected strict mode reserved word
-    at ModuleLoader.moduleStrategy(node: internal / modules / esm / translators: 169: 18)
-    at callTranslator(node: internal / modules / esm / loader: 272: 14)
-    at ModuleLoader.moduleProvider(node: internal / modules / esm / loader: 278: 30)
-    at async link(node: internal / modules / esm / module_job: 78: 21)
+    TypeError[ERR_UNKNOWN_FILE_EXTENSION]: Unknown file extension ".ts" for C: \Users\N0161651\REPO\ronin - auto - playwright - automation\scripts\generateRules.ts
+    at Object.getFileProtocolModuleFormat[as file:](node: internal/modules/esm / get_format: 160: 9)
+    at defaultGetFormat(node: internal / modules / esm / get_format: 203: 36)
+    at defaultLoad(node: internal / modules / esm / load: 143: 22)
+    at async ModuleLoader.load(node: internal / modules / esm / loader: 396: 7)
+    at async ModuleLoader.moduleProvider(node: internal / modules / esm / loader: 278: 45)
+    at async link(node: internal / modules / esm / module_job: 78: 21) {
+    code: 'ERR_UNKNOWN_FILE_EXTENSION'
+}
 
 Node.js v20.13.1
